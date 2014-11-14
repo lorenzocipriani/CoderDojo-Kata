849f7088dcf1f8b513f0c15d06a059b4b1f96869
Lorenzo Cipriani <lorenzo1974@gmail.com>
2014-11-14 07:10:10 +0000

    Add skins-CoderDojoKata-1.24

---
+++ includes/deferred/coderdojokatatutorial/resources/sidebar.tpl.php
@@ -0,0 +1,27 @@
+
+<div class="col-md-2 col-sm-3 pull-left sidebar-holder">
+	<aside id="sidebar">
+		<div class="widget kata widget_categories" id="categories-2">
+			<ul>
+				<li class="cat-item kata home"><a href="home.html">Home</a></li>
+				<li class="cat-item kata about"><a href="about.html">About Kata</a>
+				</li>
+				<li class="cat-item kata organise"><a href="about.html">Organise</a>
+					<ul class="subitem">
+						<li>How to start a dojo</li>
+						<li>Top Tips</li>
+					</ul></li>
+
+
+				<li class="cat-item kata ninjas"><a href="ninjas.html">Ninjas</a>
+					<ul class="subitem">
+						<li>Explore</li>
+					</ul></li>
+				<li class="cat-item kata mentors"><a href="mentors.html">Mentors</a>
+					<ul class="subitem">
+						<li>Tutorials</li>
+					</ul></li>
+			</ul>
+		</div>
+	</aside>
+</div>
