0c3d162bc31970e34cb556d05387fb7f1891cb4a
Lorenzo Cipriani <lorenzo1974@gmail.com>
2014-11-26 08:30:26 +0000

    Add extensions-CoderDojoKata-1.24

---
+++ extensions/CoderDojoKata/SpecialKataCourse.i18n.php
@@ -0,0 +1,8 @@
+<?php
+$messages = array();
+
+$messages['en'] = array(
+	'katacourse' => 'Kata Course'
+);
+var_dump("pippo==========>pippo");
+?>
---
+++ includes/cache/bloom/CoderDojoKata/SpecialKataMentorsTutorialList.i18n.php
@@ -0,0 +1,8 @@
+<?php
+$messages = array();
+
+$messages['en'] = array(
+	'katamentorstutoriallist' => 'Kata for Mentors - Tutorials List'
+);
+
+?>
