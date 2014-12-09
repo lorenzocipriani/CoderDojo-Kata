c5424ce9332fcd225bc74b94d5925e01a2e20236
Lorenzo Cipriani <lorenzo1974@gmail.com>
2014-12-09 13:31:25 +0000

    Update UpdateIntegration.sh

--- scripts/UpdateIntegration.sh
+++ scripts/UpdateIntegration.sh
@@ -1,27 +1,52 @@
 #!/bin/bash
-cd ~/git/CoderDojo-Kata
-git fetch --depth=1 --all
-git pull --no-commit -a -n kata-integration-1.24 REL1_24
-git commit -a -m 'Update kata-integration'
-git pull --no-commit -a -n core-1.24 REL1_24
-git commit -a -m 'Update core-1.24'
-git pull --no-commit -s subtree skins-Vector-1.24 REL1_24
-git commit -a -m 'Update skins-Vector-1.24'
-git pull --no-commit -s subtree extensions-VisualEditor-1.24 REL1_24
-git commit -a -m 'Update extensions-VisualEditor-1.2i4'
-git pull --no-commit -s subtree extensions-TemplateData-1.24 REL1_24
-git commit -a -m 'Update extensions-TemplateData-1.24'
-git pull --no-commit -s subtree extensions-SemanticDrilldown-1.24 REL1_24
-git commit -a -m 'Update extensions-SemanticDrilldown-1.24'
-git pull --no-commit -s subtree extensions-Ratings-1.24 REL1_24
-git commit -a -m 'Update extensions-Ratings-1.24'
-git pull --no-commit -s subtree extensions-Validator-1.24 REL1_24
-git commit -a -m 'Update extensions-Validator-1.24'
-git pull --no-commit -s subtree extensions-CoderDojoKata-1.24 REL1_24
-git commit -a -m 'Update extensions-CoderDojoKata-1.24'
-git pull --no-commit -s subtree skins-CoderDojoKata-1.24 REL1_24
-git commit -a -m 'Update skins-CoderDojoKata-1.24'
-git pull --no-commit -a -n kata-rel-1.24 REL1_24
-git commit -a -m 'Update kata-rel-1.24'
-git push
+
+GIT_HOME=~/git
+KATA_HOME=[set/path/to/root/folder/of/your/website]
+
+WIKI_REL=REL1_24
+WIKI_BRANCH=$WIKI_REL
+
+KATA_REL=master
+KATA_BRANCH=$KATA_REL
+
+cd $GIT_HOME/CoderDojo-Kata
+git pull --depth=1
+
+cd $GIT_HOME/CoderDojo-Kata/vendor
+git pull --recurse-submodules --depth=1
+
+cd $GIT_HOME/CoderDojo-Kata/skins/Vector
+git pull --recurse-submodules --depth=1
+
+cd $GIT_HOME/CoderDojo-Kata/extensions/Parsoid
+git pull --recurse-submodules --depth=1
+cd $GIT_HOME/CoderDojo-Kata/extensions/TemplateData
+git pull --recurse-submodules --depth=1
+cd $GIT_HOME/CoderDojo-Kata/extensions/VisualEditor
+git pull --recurse-submodules --depth=1
+
+cd $GIT_HOME/CoderDojo-Kata/Kata
+git pull --depth=1
+
+cd $GIT_HOME/CoderDojo-Kata-skins/CoderDojoKata
+git pull --depth=1
+cd $GIT_HOME/CoderDojo-Kata-extensions
+git pull --depth=1
+
+mv $GIT_HOME/CoderDojo-Kata-skins/CoderDojoKata/coderdojokata $GIT_HOME/CoderDojo-Kata-skins/CoderDojoKata/CoderDojoKata
+
+rm -rf $GIT_HOME/CoderDojo-Kata/skins/CoderDojoKata
+cp -r $GIT_HOME/CoderDojo-Kata-skins/CoderDojoKata/CoderDojoKata $GIT_HOME/CoderDojo-Kata/skins
+
+rm -rf $GIT_HOME/CoderDojo-Kata/extensions/CoderDojoKata
+cp -r $GIT_HOME/CoderDojo-Kata-extensions/CoderDojoKata $GIT_HOME/CoderDojo-Kata/extensions
+
+rm -rf $GIT_HOME/CoderDojo-Kata/extensions/W4G
+cp -r $GIT_HOME/CoderDojo-Kata-extensions/W4G $GIT_HOME/CoderDojo-Kata/extensions
+
+cd $GIT_HOME
+
+echo -e "Now you can sync (cp, rsync, ftp, git-ftp, etc.) your ${GIT_HOME}/CoderDojo-Kata folder on ${KATA_HOME} folder\n"
+
+#git push
 
