afc42b2f350ee7a1a8172912eb3d22106851de56
Lorenzo Cipriani <lorenzo1974@gmail.com>
2015-02-13 19:05:58 +0000

    Update UpdateIntegration.sh

--- scripts/UpdateIntegration.sh
+++ scripts/UpdateIntegration.sh
@@ -1,52 +1,52 @@
 #!/bin/bash
 
-GIT_HOME=~/git
-KATA_HOME="[set/path/to/root/folder/of/your/website]"
+source ./build.properties
 
-WIKI_REL=REL1_24
-WIKI_BRANCH=$WIKI_REL
+#CoderDojo-Kata/ CoderDojo-Kata-extensions/ CoderDojo-Kata-skins/
+#Kata/  core/ extensions/ kata-extensions/ kata-skins/ skins/ vendor/
 
-KATA_REL=master
-KATA_BRANCH=$KATA_REL
+echo -e "\nUpdating $GIT_HOME/CoderDojo-Kata/core\n"
+cd $GIT_HOME/CoderDojo-Kata/core
+git $SSL pull --depth=1
 
-cd $GIT_HOME/CoderDojo-Kata
-git pull --depth=1
-
+echo -e "\nUpdating $GIT_HOME/CoderDojo-Kata/vendor\n"
 cd $GIT_HOME/CoderDojo-Kata/vendor
-git pull --recurse-submodules --depth=1
+git $SSL pull --depth=1 --recurse-submodules
 
+echo -e "\nUpdating $GIT_HOME/CoderDojo-Kata/skins/Vector\n"
 cd $GIT_HOME/CoderDojo-Kata/skins/Vector
-git pull --recurse-submodules --depth=1
+git $SSL pull --depth=1 --recurse-submodules
 
-cd $GIT_HOME/CoderDojo-Kata/extensions/ParserFunctions
-git pull --recurse-submodules --depth=1
-cd $GIT_HOME/CoderDojo-Kata/extensions/Parsoid
-git pull --recurse-submodules --depth=1
-cd $GIT_HOME/CoderDojo-Kata/extensions/TemplateData
-git pull --recurse-submodules --depth=1
-cd $GIT_HOME/CoderDojo-Kata/extensions/VisualEditor
-git pull --recurse-submodules --depth=1
+echo -e "\nUpdating $GIT_HOME/CoderDojo-Kata/extensions/*\n"
+cd $GIT_HOME/CoderDojo-Kata/extensions
 
+for D in *
+do
+  if [ -d "${D}" ]
+  then
+    echo -e "\nUpdating $GIT_HOME/CoderDojo-Kata/extensions/${D}\n"
+    cd $GIT_HOME/CoderDojo-Kata/extensions/$D
+    git $SSL pull --depth=1 --recurse-submodules
+    cd $GIT_HOME/CoderDojo-Kata/extensions
+  fi
+done
+
+echo -e "\nUpdating $GIT_HOME/CoderDojo-Kata/Kata\n"
 cd $GIT_HOME/CoderDojo-Kata/Kata
-git pull --depth=1
+git $SSL pull --depth=1 --recurse-submodules
 
-cd $GIT_HOME/CoderDojo-Kata-skins/CoderDojoKata
-git pull --depth=1
-cd $GIT_HOME/CoderDojo-Kata-extensions
-git pull --depth=1
+echo -e "\nUpdating $GIT_HOME/CoderDojo-Kata/kata-skins\n"
+cd $GIT_HOME/CoderDojo-Kata/kata-skins
+git $SSL pull --depth=1 --recurse-submodules
 
-rm -rf $GIT_HOME/CoderDojo-Kata/skins/CoderDojoKata
-cp -r $GIT_HOME/CoderDojo-Kata-skins/CoderDojoKata/CoderDojoKata $GIT_HOME/CoderDojo-Kata/skins
+echo -e "\nUpdating $GIT_HOME/CoderDojo-Kata/kata-extensions\n"
+cd $GIT_HOME/CoderDojo-Kata/kata-extensions
+git $SSL pull --depth=1 --recurse-submodules
 
-rm -rf $GIT_HOME/CoderDojo-Kata/extensions/CoderDojoKata
-cp -r $GIT_HOME/CoderDojo-Kata-extensions/CoderDojoKata $GIT_HOME/CoderDojo-Kata/extensions
+cd $WORKING_DIR
 
-rm -rf $GIT_HOME/CoderDojo-Kata/extensions/W4G
-cp -r $GIT_HOME/CoderDojo-Kata-extensions/W4G $GIT_HOME/CoderDojo-Kata/extensions
+echo -e "Now you can build the integration running\n"
+echo -e "bash ${WORKING_DIR}/BuildIntegration.sh\n"
 
-cd $GIT_HOME
-
-echo -e "Now you can sync (cp, rsync, ftp, git-ftp, etc.) your ${GIT_HOME}/CoderDojo-Kata folder on ${KATA_HOME} folder\n"
-
-#git push
+#bash ${WORKING_DIR}/BuildIntegration.sh
 
