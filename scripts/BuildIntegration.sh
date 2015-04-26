27b0129921f1a3d0c5cdf3493beb37f51d94a80e
Lorenzo Cipriani <lorenzo1974@gmail.com>
2015-04-26 15:25:34 +0100

    Update BuildIntegration.sh

--- scripts/BuildIntegration.sh
+++ scripts/BuildIntegration.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-GIT_HOME=~/git
-KATA_BUILD=[/set/path/to/build/folder]
-KATA_HOME=[/set/path/to/root/folder/of/your/website]
-PHP_CLI=[/set/path/to/root/folder/of/your/php/cli]
-
-WORKING_DIR=$PWD
+source ./build.properties
 
 if [ ! -d "${GIT_HOME}/CoderDojo-Kata" ]
 then
@@ -22,11 +17,28 @@ rsync -a --exclude '.git' --exclude '.git*' --exclude '.js*' --exclude '.travis*
 rsync -a --exclude '.git' --exclude '.git*' --exclude '.js*' --exclude '.travis*' ${GIT_HOME}/CoderDojo-Kata/skins ${KATA_BUILD}/
 rsync -a --exclude '.git' --exclude '.git*' --exclude '.js*' --exclude '.travis*' ${GIT_HOME}/CoderDojo-Kata/extensions ${KATA_BUILD}/
 rsync -a --exclude '.git' --exclude '.git*' --exclude '.js*' --exclude '.travis*' ${GIT_HOME}/CoderDojo-Kata/Kata ${KATA_BUILD}/
-rsync -a --exclude '.git' --exclude '.git*' --exclude '.js*' --exclude '.travis*' ${GIT_HOME}/CoderDojo-Kata/kata-skins/CoderDojoKata ${KATA_BUILD}/skins/
+rsync -a --exclude '.git' --exclude '.git*' --exclude '.js*' --exclude '.travis*' ${GIT_HOME}/CoderDojo-Kata/kata-skins/coderdojokata ${KATA_BUILD}/skins/
 rsync -a --exclude '.git' --exclude '.git*' --exclude '.js*' --exclude '.travis*' ${GIT_HOME}/CoderDojo-Kata/kata-extensions/* ${KATA_BUILD}/extensions/
-rsync -a --exclude '.git' --exclude '.git*' --exclude '.js*' --exclude '.travis*' ${GIT_HOME}/CoderDojo-Kata/composer/* ${KATA_BUILD}/
+#rsync -a --exclude '.git' --exclude '.git*' --exclude '.js*' --exclude '.travis*' ${GIT_HOME}/CoderDojo-Kata/composer/* ${KATA_BUILD}/
 
-cd $GIT_HOME/CoderDojo-Kata
+if [ -d "$KATA_BUILD/skins/coderdojokata" ]
+then
+    if [ -d "$KATA_BUILD/skins/CoderDojoKata" ]
+    then
+        rm -rf $KATA_BUILD/skins/CoderDojoKata
+    fi
+    mv $KATA_BUILD/skins/coderdojokata $KATA_BUILD/skins/CoderDojoKata
+fi
+
+if [ ! -d "$KATA_BUILD/composer" ]
+then
+    mkdir $KATA_BUILD/composer
+fi
+cd $KATA_BUILD/composer
+$PHP_CLI -r "readfile('https://getcomposer.org/installer');" | $PHP_CLI
+cd $WORKING_DIR
+
+cd $KATA_BUILD
 echo -e "Composer: installing semantic-media-wiki"
 $PHP_CLI composer.phar require mediawiki/semantic-media-wiki "~2.1"
 echo -e "Composer: installing semantic-extra-special-properties"
@@ -41,4 +53,3 @@ cd $WORKING_DIR
 
 echo -e "Now you can sync (cp, rsync, ftp, git-ftp, etc.) your ${KATA_BUILD} folder on ${KATA_HOME} folder.\n"
 echo -e "E.g.:\nrsync -a ${KATA_BUILD}/* ${KATA_HOME}\n"
-
